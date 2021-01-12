ce71b61f56b5500f4f5d9028d14a1fbd523671c9
C4TRIS <59656357+C4TRIS@users.noreply.github.com>
2021-01-12 23:09:40 +0600

    Add files via upload

---
+++ classes/Config.php
@@ -0,0 +1,8 @@
+<?php
+    abstract class Config {
+        const HOST = 'localhost';
+        const DB_NAME = 'dsa';
+        const DB_USER = 'root';
+        const DB_PASSWORD = 'root';
+    }
+?>
